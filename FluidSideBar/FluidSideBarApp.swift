0b51cf8fe2a153cec422b9a4a0a699d5d0c771f8
Bruna Baudel <brubaudel@gmail.com>
2024-10-12 08:26:17 +0200

    Initial Commit

---
+++ FluidSideBar/FluidSideBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluidSideBarApp.swift
+//  FluidSideBar
+//
+//  Created by Bruna on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FluidSideBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
